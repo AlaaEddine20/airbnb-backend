407e4d8c2827285c41873791ddf388cdbe8da080
Alaa Eddine <alaneddin91@gmail.com>
2021-01-06 17:45:12 +0100

    adds edit place

--- src/services/places/Places.js
+++ src/services/places/Places.js
@@ -24,6 +24,62 @@ const storage = new CloudinaryStorage({
 
 const cloudinaryStorage = multer({ storage: storage });
 
+// POST PLACES BY OWNER ID
+placesRouter.post(
+  "/:owner_id/delete",
+  validationPlaces,
+  async (req, res, next) => {
+    try {
+      const validationErrors = validationResult(req);
+
+      const places = await getPlaces();
+      const owners = await getOwners();
+      if (!validationErrors.isEmpty()) {
+        const error = new Error();
+        error.message = validationErrors;
+        error.httpStatusCode = 400;
+        return next(error);
+      }
+      // CHECKING IF THE PLACE EXISTS IN THE DB BY THE ADDRESS
+      const streetAddress = req.body.address.street;
+      const place = places.find(
+        (place) => place.address.street === streetAddress
+      );
+      // IF THE ADDRESS EXISTS RETURN...
+      if (place) {
+        return next(new Error("Place already exists!"));
+      }
+
+      // CHECKING IF THE OWNER EXISTS IN DB
+      const owner = owners.find(
+        (owner) => owner.owner_id === req.params.owner_id
+      );
+
+      if (!owner) {
+        const error = new Error();
+        error.httpStatusCode = 400;
+        error.message = "Owner not found!";
+        next(error);
+      }
+      // AFTER THE CHECKS ARE PASSED THEN POST
+      const newPlace = {
+        ...req.body,
+        place_pics: [],
+        owner_id: req.params.owner_id,
+        ownerName: owner.UserName,
+        place_id: uniqid(),
+        createdAt: new Date(),
+      };
+      places.push(newPlace);
+      await writePlaces(places);
+      res.status(201).send("Successfully posted");
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+);
+
 // GET ALL PLACES
 placesRouter.get("/", async (req, res, next) => {
   try {
@@ -36,60 +92,8 @@ placesRouter.get("/", async (req, res, next) => {
   }
 });
 
-// POST PLACES BY OWNER ID
-placesRouter.post("/:owner_id", validationPlaces, async (req, res, next) => {
-  try {
-    const validationErrors = validationResult(req);
-
-    const places = await getPlaces();
-    const owners = await getOwners();
-    if (!validationErrors.isEmpty()) {
-      const error = new Error();
-      error.message = validationErrors;
-      error.httpStatusCode = 400;
-      return next(error);
-    }
-    // CHECKING IF THE PLACE EXISTS IN THE DB BY THE ADDRESS
-    const streetAddress = req.body.address.street;
-    const place = places.find(
-      (place) => place.address.street === streetAddress
-    );
-    // IF THE ADDRESS EXISTS RETURN...
-    if (place) {
-      return next(new Error("Place already exists!"));
-    }
-
-    // CHECKING IF THE OWNER EXISTS IN DB
-    const owner = owners.find(
-      (owner) => owner.owner_id === req.params.owner_id
-    );
-
-    if (!owner) {
-      const error = new Error();
-      error.httpStatusCode = 400;
-      error.message = "Owner not found!";
-      next(error);
-    }
-    // AFTER THE CHECKS ARE PASSED THEN POST
-    const newPlace = {
-      ...req.body,
-      place_pics: [],
-      owner_id: req.params.owner_id,
-      ownerName: owner.UserName,
-      place_id: uniqid(),
-      createdAt: new Date(),
-    };
-    places.push(newPlace);
-    await writePlaces(places);
-    res.status(201).send("Successfully posted");
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-});
-
 // DELETE PLACE
-placesRouter.delete("/:place_id", validationPlaces, async (req, res, next) => {
+placesRouter.delete("/:place_id", async (req, res, next) => {
   try {
     const places = await getPlaces();
     // FIND PLACE
@@ -104,6 +108,33 @@ placesRouter.delete("/:place_id", validationPlaces, async (req, res, next) => {
       );
       await writePlaces(filteredPlaces);
       res.status(204).send("Deleted!");
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+// EDIT PLACE
+placesRouter.put("/:place_id", validationPlaces, async (req, res, next) => {
+  try {
+    const places = await getPlaces();
+
+    const placeIndex = places.findIndex(
+      (place) => place.place_id === req.params.place_id
+    );
+    if (placeIndex !== -1) {
+      const updatedPlace = [
+        ...places.slice(0, placeIndex),
+        { ...places[placeIndex], ...req.body },
+        ...places.slice(placeIndex + 1),
+      ];
+      await writePlaces(updatedPlace);
+      res.send("Updated!");
+    } else {
+      err = new Error();
+      err.httpStatusCode = 404;
+      next(err);
     }
   } catch (error) {
     console.log(error);
